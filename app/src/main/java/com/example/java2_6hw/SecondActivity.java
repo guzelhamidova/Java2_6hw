ddd6751e4842bbbd1cd3b534e5e32f0d9a7a3358
guzel <guzel.hamidova07@mail.ru>
2021-05-10 18:49:39 +0600

    gf

--- app/src/main/java/com/example/java2_6hw/SecondActivity.java
+++ app/src/main/java/com/example/java2_6hw/SecondActivity.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
     }
 }
--- app/src/main/java/com/example/java2_6hw/ThirdActivity.java
+++ app/src/main/java/com/example/java2_6hw/ThirdActivity.java
@@ -2,17 +2,13 @@ package com.example.java2_6hw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_third);
     }
 }
